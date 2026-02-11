08c7f4bf7d98148fe37ec87e906249fdf5bec92e
aflinch <alexanderflinchum@gmail.com>
2026-02-10 21:51:51 -0600

    implimented removeElement

--- leet_code_easy/RemoveElement.cs
+++ leet_code_easy/RemoveElement.cs
@@ -1,9 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+// Given an integer array nums and an integer val, remove all occurrences of val in nums in-place.
+// The order of the elements may be changed. Then return the number of elements in nums which are not equal
+// to val.
+
+// Consider the number of elements in nums which are not equal to val be k, to get accepted,
+// you need to do the following things:
+//
+// Change the array nums such that the first k elements of nums contain the elements which
+// are not equal to val. The remaining elements of nums are not important as well as the size of nums.
+//
+// Return k.
+
+
 namespace leet_code_easy
 {
-    internal class RemoveElement
+    public class RemoveElement
     {
-        public static void Main(string[] args)
+        public static int RemoveElementFromList(int[] nums, int val) {
+            int k = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    int temp = nums[k];
+                    nums[k] = nums[i];
+                    nums[i] = temp;
+                    k++;
+                }
+            }
+
+            return k;
+        }
+
+        public static void Main()
         {
+            int[] arr = { 0, 1, 3, 0, 2, 2, 4, 2 };
+            var val = 2;
+            Console.WriteLine(RemoveElementFromList(arr, val));
         }
     }
 }
