897e670aed39aa7d3e0bf57833fdac7627107f96
aflinch <alexanderflinchum@gmail.com>
2026-02-14 19:01:27 -0600

    Implimented LengthOfLastWord

---
+++ leet_code_easy/LengthLastWord.cs
@@ -0,0 +1,29 @@
+﻿namespace leet_code_easy
+{
+    public class LengthLastWord
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            var charFound = false;
+            var len = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (char.IsLetter(s[i]))
+                {
+                    charFound = true;
+                    len++;
+                }
+                else
+                {
+                    if (charFound)
+                    {
+                        return len;
+                    }
+                }
+            }
+            
+            return len;
+        }
+    }
+}
